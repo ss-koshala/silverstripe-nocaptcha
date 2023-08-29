643331086772a57b065d9588cf65fb9ceee21d5d
ss-koshala <koshala.manojeewa@silverstripers.com>
2023-08-29 14:56:12 +0530

    amp fix one

--- src/Forms/NocaptchaField.php
+++ src/Forms/NocaptchaField.php
@@ -8,6 +8,8 @@ use SilverStripe\Forms\FormField;
 use SilverStripe\View\Requirements;
 use SilverStripe\Control\Controller;
 use SilverStripe\Core\Injector\Injector;
+use SilverStripers\AMP\Control\AMPDirector;
+
 
 
 class NocaptchaField extends FormField {
@@ -246,7 +248,12 @@ class NocaptchaField extends FormField {
     {
         Requirements::customCSS('.nocaptcha { display: none !important; }', self::class);
         if ($this->getHandleSubmitEvents()) {
-            Requirements::javascript('https://www.google.com/recaptcha/api.js?render=' . urlencode($this->getSiteKey()) . '&onload=noCaptchaFormRender');
+
+            if (AMPDirector::is_amp()) {
+                Requirements::includeInHTML('<amp-script layout="container" src="https://www.google.com/recaptcha/api.js?render='.urlencode($this->getSiteKey()).'&onload=noCaptchaFormRender" class="recaptcha"></amp-script>');
+            } else {
+                Requirements::javascript('https://www.google.com/recaptcha/api.js?render=' . urlencode($this->getSiteKey()) . '&onload=noCaptchaFormRender');
+            }
             Requirements::javascript('undefinedoffset/silverstripe-nocaptcha:javascript/NocaptchaField_v3.js');
 
             $form = $this->getForm();
